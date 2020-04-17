f80f2fb6839e538de467106d4dc03bafc7d8b26e
779g <aztlan@779.mx>
2020-04-17 15:18:33 +0000

    wrap the pt

--- webpack.client.prod.js
+++ webpack.client.prod.js
@@ -87,7 +87,7 @@ module.exports = {
             const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
 
             // npm package names are URL-safe, but some servers don't like @ symbols
-            return `npm.${packageName.replace('@', '')}`
+            return `m-${packageName.replace('@', '')}`
           }
         }
       }
