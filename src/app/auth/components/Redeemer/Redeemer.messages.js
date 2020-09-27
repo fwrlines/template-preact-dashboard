91341af8c7aecb256e64ff6f7581b877dca92494
779g <47156835+779g@users.noreply.github.com>
2020-09-27 10:52:09 +0200

    Update Redeemer.messages.js

--- src/app/auth/components/Redeemer/Redeemer.messages.js
+++ src/app/auth/components/Redeemer/Redeemer.messages.js
@@ -3,12 +3,12 @@ import { defineMessages } from 'react-intl'
 
 export default defineMessages({
   loggingin:{
-    id            :'app.auth.component.redeemer.loggingin',
+    id            :'app.auth.components.redeemer.loggingin',
     defaultMessage:'Logging in',
     description   :''
   },
   redirect_to:{
-    id            :'app.auth.component.redeemer.redirect_to',
+    id            :'app.auth.components.redeemer.redirect_to',
     defaultMessage:'Will redirect to',
     description   :''
   }
